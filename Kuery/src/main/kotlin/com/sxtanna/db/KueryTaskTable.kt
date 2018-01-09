77d9b4b690f1b2e9a8cb3ae41d9e77d5b4bc5984
Sxtanna <ranald.taylor@gmail.com>
2018-01-08 22:34:04 -0500

    Change generic letters

--- Kuery/src/main/kotlin/com/sxtanna/db/KueryTaskTable.kt
+++ Kuery/src/main/kotlin/com/sxtanna/db/KueryTaskTable.kt
@@ -9,7 +9,7 @@ import kotlin.reflect.KProperty1
 /**
  * A [KueryTask] specific for the supplied [Table]
  */
-class KueryTaskTable<E : Any>(private val table : Table<E>, private val task : KueryTask) : Creator.TableCreator<E>, Deleter.TableDeleter<E>, Dropper.TableDropper<E>, Inserter.TableInserter<E>, Selector.TableSelector<E>, Truncater.TableTruncater<E>, Updater.TableUpdater<E> {
+class KueryTaskTable<T : Any>(private val table : Table<T>, val task : KueryTask) : DBCreator.TableCreator<T>, DBDeleter.TableDeleter<T>, DBDropper.TableDropper<T>, DBInserter.TableInserter<T>, DBSelector.TableSelector<T>, DBTruncater.TableTruncater<T>, DBUpdater.TableUpdater<T> {
 
     //region Create statement
     override fun create() = task.create(table)
@@ -17,11 +17,11 @@ class KueryTaskTable<E : Any>(private val table : Table<E>, private val task : K
 
 
     //region Delete statements
-    override fun delete() : Delete<E> {
+    override fun delete() : Delete<T> {
         return task.delete(table)
     }
 
-    override fun delete(rows : Collection<E>) {
+    override fun delete(rows : Collection<T>) {
         task.delete(table, rows)
     }
 
@@ -39,11 +39,11 @@ class KueryTaskTable<E : Any>(private val table : Table<E>, private val task : K
 
 
     //region Insert statements
-    override fun insert(rows : Collection<E>) {
+    override fun insert(rows : Collection<T>) {
         task.insert(table, rows)
     }
 
-    override fun insert(duplicate : Duplicate<Table<E>>, rows : Collection<E>) {
+    override fun insert(duplicate : Duplicate<Table<T>>, rows : Collection<T>) {
         task.insert(table, duplicate, rows)
     }
     //endregion
@@ -53,29 +53,29 @@ class KueryTaskTable<E : Any>(private val table : Table<E>, private val task : K
     override fun select() = task.select(table)
 
     override fun <R1 : Any?>
-          select(prop : KProperty1<E, R1>) = task.select(table, prop)
+          select(prop : KProperty1<T, R1>) = task.select(table, prop)
 
     override fun <R1 : Any?, R2 : Any?>
-          select(prop1 : KProperty1<E, R1>,
-                 prop2 : KProperty1<E, R2>) = task.select(table, prop1, prop2)
+          select(prop1 : KProperty1<T, R1>,
+                 prop2 : KProperty1<T, R2>) = task.select(table, prop1, prop2)
 
     override fun <R1 : Any?, R2 : Any?, R3 : Any?>
-          select(prop1 : KProperty1<E, R1>,
-                 prop2 : KProperty1<E, R2>,
-                 prop3 : KProperty1<E, R3>) = task.select(table, prop1, prop2, prop3)
+          select(prop1 : KProperty1<T, R1>,
+                 prop2 : KProperty1<T, R2>,
+                 prop3 : KProperty1<T, R3>) = task.select(table, prop1, prop2, prop3)
 
     override fun <R1 : Any?, R2 : Any?, R3 : Any?, R4 : Any?>
-          select(prop1 : KProperty1<E, R1>,
-                 prop2 : KProperty1<E, R2>,
-                 prop3 : KProperty1<E, R3>,
-                 prop4 : KProperty1<E, R4>) = task.select(table, prop1, prop2, prop3, prop4)
+          select(prop1 : KProperty1<T, R1>,
+                 prop2 : KProperty1<T, R2>,
+                 prop3 : KProperty1<T, R3>,
+                 prop4 : KProperty1<T, R4>) = task.select(table, prop1, prop2, prop3, prop4)
 
     override fun <R1 : Any?, R2 : Any?, R3 : Any?, R4 : Any?, R5 : Any?>
-          select(prop1 : KProperty1<E, R1>,
-                 prop2 : KProperty1<E, R2>,
-                 prop3 : KProperty1<E, R3>,
-                 prop4 : KProperty1<E, R4>,
-                 prop5 : KProperty1<E, R5>) = task.select(table, prop1, prop2, prop3, prop4, prop5)
+          select(prop1 : KProperty1<T, R1>,
+                 prop2 : KProperty1<T, R2>,
+                 prop3 : KProperty1<T, R3>,
+                 prop4 : KProperty1<T, R4>,
+                 prop5 : KProperty1<T, R5>) = task.select(table, prop1, prop2, prop3, prop4, prop5)
     //endregion
 
 
@@ -87,17 +87,17 @@ class KueryTaskTable<E : Any>(private val table : Table<E>, private val task : K
 
 
     //region Update statements
-    override fun update() : Update<E> = task.update(table)
+    override fun update() : Update<T> = task.update(table)
 
-    override fun update(rows : Collection<E>) = task.update(table, rows)
+    override fun update(rows : Collection<T>) = task.update(table, rows)
 
-    override fun update(vararg values : Value<E, *>) = task.update(table, *values)
+    override fun update(vararg values : Value<T, *>) = task.update(table, *values)
 
-    override fun updateAllRows(row : E) {
+    override fun updateAllRows(row : T) {
         task.updateAllRows(table, row)
     }
 
-    override fun updateAllRows(vararg values : Value<E, *>) {
+    override fun updateAllRows(vararg values : Value<T, *>) {
         task.updateAllRows(table, *values)
     }
     //endregion
